d204ba05987395fe211082af4e8821149bfee16d
Alexandre <alexandre.jeunot-caire@grenoble-inp.org>
2021-06-19 21:37:54 +0200

    comments added

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 from cv2 import cv2
 from collections import deque
 import mediapipe as mp
-from time import time
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
@@ -22,14 +21,6 @@ tips = {"thumb": 4,
         "ring": 16,
         "little": 20}
 
-def is_open(res, finger):
-    if tips[finger] == 4:
-        return res[tips[finger]][1] < res[tips[finger] - 1][1]
-    return res[tips[finger]][2] < res[tips[finger] - 2][2]
-
-def show(name):
-    return cv2.imread(name)
-
 rock_img = cv2.imread("img/rock.png")
 paper_img = cv2.imread("img/paper.png")
 scissors_img = cv2.imread("img/scissors.png")
@@ -37,20 +28,28 @@ scissors_img = cv2.imread("img/scissors.png")
 history = deque()
 
 with mp_hands.Hands(
-        min_detection_confidence=0.75,
-        min_tracking_confidence=0.75) as hands:
-    ongoing = True
-    time_to_decide = True
-    last_played = None
-    last_played_name = None
-    true_answer = None
-    true_answer_name = None
+        min_detection_confidence=0.7,
+        min_tracking_confidence=0.7) as hands:
+    
+    # Game parameters
+    ongoing = True # Is the round ongoing
+    time_to_decide = True # 
+    last_played = None # What the bot played
+    last_played_name = None # Name of the movement the bot made
+    true_answer = None # What the player actually did
+    true_answer_name = None # Name of the movement the player did
+
+    # Where to place the graphic elements
+
     x_offest = 500
     x_offest_user = 0
     y_offset = 350
     user_score = 0
     bot_score = 0
+
     while cap.isOpened():
+
+        # Treating the image in order to recognize the hands
         _, image = cap.read()
    
         image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
@@ -59,18 +58,23 @@ with mp_hands.Hands(
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         res = []
-        if ongoing and results.multi_hand_landmarks:
+        if ongoing and results.multi_hand_landmarks: # If the round is not over (and we detected hands)
+            height, width, _ = image.shape
             right_hand = results.multi_hand_landmarks[0]
+
+            # Position of all the joints in the hand
             for i, lm in enumerate(right_hand.landmark):
-                res.append((i, int(lm.x * image.shape[0]), int(lm.y * image.shape[1])))
+                res.append((i, lm.x * height, lm.y * width))
 
 
-            fingers_open = [res[4][1] < res[3][1],
-                            res[8][2] < res[6][2],
-                            res[12][2] < res[10][2],
-                            res[16][2] < res[14][2],
-                            res[20][2] < res[18][2]
+            fingers_open = [res[4][1] < res[3][1], #true if thumb is open
+                            res[8][2] < res[6][2], #true if index is open
+                            res[12][2] < res[10][2], #true if middle finger is open
+                            res[16][2] < res[14][2], #true if ring finger is open
+                            res[20][2] < res[18][2] #true if little finger is open
                             ]
+
+            # Determine what the player most likely played
             candidate = ROCK
             if fingers_open[3] or fingers_open[4]:
                 candidate = PAPER
@@ -79,7 +83,7 @@ with mp_hands.Hands(
 
             history.append(candidate)
 
-            if time_to_decide and len(history) >= 3:
+            if time_to_decide and len(history) >= 3: # This is to be more accurate in our decision. In particular, people tend to hide their fist behind their head, and it influences the first frames
                 res = None
                 if history.count(ROCK) == 3:
                     res = ROCK
@@ -98,10 +102,14 @@ with mp_hands.Hands(
                     else:
                         last_played = rock_img
                     time_to_decide = False
+
             else:
+                # We draw the hand
                 for hand_landmarks in results.multi_hand_landmarks:
                     mp_drawing.draw_landmarks(
                         image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                
+                # We get what the player did with more accuracy
                 if len(history) == 5:
                     res = None
                     if history.count(ROCK) == 5:
@@ -120,7 +128,8 @@ with mp_hands.Hands(
                         else: 
                             true_answer = scissors_img
                         ongoing = False
-                    
+            
+            # We show the image the AI """"guessed""""" ;p
             if last_played is not None:
                 image[y_offset:y_offset+last_played.shape[0], x_offest:x_offest+last_played.shape[1]] = last_played
 
@@ -128,6 +137,7 @@ with mp_hands.Hands(
         if last_played is not None:
             image[y_offset:y_offset+last_played.shape[0], x_offest:x_offest+last_played.shape[1]] = last_played
 
+        # Once we determined with accuracy the true movement, we show it too
         if true_answer is not None:
             cv2.putText(image, "Round over", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 0), 10)
             cv2.putText(image, "Press \"[SPACE]\" to continue", (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
@@ -137,17 +147,21 @@ with mp_hands.Hands(
         cv2.putText(image, f"You: {user_score}", (x_offest_user, y_offset - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         cv2.putText(image, f"Bot: {bot_score}", (x_offest, y_offset - 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
         cv2.imshow('Rock paper scissors', image)
-        if cv2.waitKey(5) & 0xFF == 32:
+
+        if cv2.waitKey(5) & 0xFF == 32: # 32 = space, 27 = esc if you want to change
             if not ongoing:
                 if true_answer_name == WINS[last_played_name]:
                     user_score += 1
                 elif last_played_name == WINS[true_answer_name]:
                     bot_score += 1
+            
+            # reset parameters
             time_to_decide = True
             ongoing = True
             last_played = None
             last_played_name = None
-            true_answer = None_name = None
+            true_answer = None
+            true_answer_name = None
             history = deque()
 
 
